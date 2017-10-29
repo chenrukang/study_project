4bc69331681c55a1924f65612a81d84700796496
chenrukang <164428334@qq.com>
2017-10-29 17:35:22 +0800

    第一次更新

---
+++ app/src/main/java/example/com/property_animation/demoActivity.java
@@ -0,0 +1,17 @@
+package example.com.property_animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Administrator on 2017/10/29 0029.
+ */
+public class demoActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_demo);
+        //
+    }
+}
